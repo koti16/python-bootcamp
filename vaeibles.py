876be4148b6713c57ff5f0e791edfa97777ad0a2
koti16 <197651399+koti16@users.noreply.github.com>
2025-12-17 00:05:38 +0530

    Add files via upload

---
+++ vaeibles.py
@@ -0,0 +1,12 @@
+# len function
+name = "koti"
+print(name)
+length=len(name)
+print(length)
+
+#project
+
+print("welcome to the band Name Generator.")
+city=input("which city did you grow up in?\n")
+pet =input("what is the name of a pet?\n")
+print("Your band name could be:"+city+" "+ pet)
