25ffbe14da75abc6931ec19f415362b21599e4b1
koti16 <197651399+koti16@users.noreply.github.com>
2025-12-25 15:06:21 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,19 @@
+student_marks = [123,34,5,65,67,765,334,542,35,32,34535,3534]
+print(max(student_marks))
+max=0
+min = 0
+for socres in student_marks :
+    if socres < max :
+        max+= socres
+        print(max)
+#range ffunction:
+#sytnax:-
+#     for number in range (a,b):
+#         print(number)
+total = 0
+for number in range(1,101):
+    total += number
+    print(total)
+
+        
+    
